ed215df3d1560015aceb7cd01cff5356b961384b
dem123456789 <diao_em@hotmail.com>
2024-12-19 14:29:17 +0800

    direct mapping not working

--- src/model/semantic.py
+++ src/model/semantic.py
@@ -4,6 +4,7 @@ from .model import normalize
 from .loss import make_loss
 from module import filter_args
 
+
 class Semantic(nn.Module):
     def __init__(self, core, tokenizer, index, cfg):
         super().__init__()
@@ -14,7 +15,8 @@ class Semantic(nn.Module):
         self.cfg = cfg
         self.data_mode = cfg['data_mode']
         self.task_mode = 'classification'
-        self.out_proj = nn.Linear(cfg[cfg['model_name']]['hidden_size'], tokenizer.vocab_size) # TODO: use bert default out proj
+        # TODO: use bert default out proj
+        self.out_proj = nn.Linear(cfg[cfg['model_name']]['hidden_size'], tokenizer.vocab_size)
 
     def freeze(self, model):
         for param in model.parameters():
@@ -30,14 +32,15 @@ class Semantic(nn.Module):
         output['pred'], output['pred_semantic'] = self.make_pred(x.last_hidden_state)
         input['target'] = target
         output['loss'] = make_loss(output, input, mode=self.task_mode, log_prob=False)
-        print(input['target_semantic'])
-        print(output['pred_semantic'])
-        exit()
+        input['target'] = input['target_semantic']
+        output['pred'] = output['pred_semantic']
+        print(output['pred'])
         return output
 
     def make_target(self, input):
         if self.cfg['mask_mode'] == 'target':
             target = input['input_ids'][:, -1].clone().detach()
+            print(target)
             input['input_ids'][:, -1][:] = self.tokenizer.mask_token_id
             input['attention_mask'][:, -1][:] = 1
         return input, target
@@ -51,9 +54,15 @@ class Semantic(nn.Module):
         if self.cfg['mask_mode'] == 'target':
             hidden_state = hidden_state.view(hidden_state.size(0), -1, self.cfg['max_length'], hidden_state.size(-1))
             hidden_state = hidden_state[:, -1]
-        pred = self.out_proj(hidden_state).transpose(1, 2)
+        pred = self.out_proj(hidden_state)
+        pred = pred.transpose(1, 2)
+        # print(pred.size())
         pred_tokens = torch.argmax(pred, dim=1)
+        print(pred_tokens)
+        # print(pred_tokens.size())
         pred_semantic = self.tokenizer.batch_decode(pred_tokens, skip_special_tokens=True)
+        print(pred_semantic)
+        # exit()
         return pred, pred_semantic
 
 
